3476c9ab7a0daecad49879fda43968c8bb06db02
John Grande <jgrande@wfrostfire.local>
2014-04-22 15:51:18 +0800

    js fix

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ core = core || {};
 core.home = function(){
   var self = core;
   for(i in self){
-    typeof self[i]['init'] != "undefined" ? self[i]['init']() || "";
+    typeof self[i]['init'] != "undefined" ? self[i]['init']() : "";
   }
 }
