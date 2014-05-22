a8652d3c1a8a69508140097f34ba0c6e54a62182
John Grande <jgrande@visualjazz.com>
2014-05-23 05:50:18 +0800

    new changes

--- js/dimensions.js
+++ js/dimensions.js
@@ -44,7 +44,7 @@ core.dimensions.prototype.setSquares = function(){
         var url = val['media']['m'];
         var img = jQuery('img',{ 'src': url });
 
-        jQuery('.bg-squares').eq(1).append(img);
+        jQuery('.bg-squares').eq(id).append(img);
       });
     }
 
