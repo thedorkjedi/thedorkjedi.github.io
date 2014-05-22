4e2acb2c1ea32e109e0095b886fd359cfdffe0eb
John Grande <jgrande@visualjazz.com>
2014-05-23 05:52:35 +0800

    new changes

--- js/dimensions.js
+++ js/dimensions.js
@@ -42,7 +42,7 @@ core.dimensions.prototype.setSquares = function(){
     function setImgURLS(obj){
       jQuery.each(obj,function(id,val){
         var url = val['media']['m'];
-        var img = jQuery('<img>',{ 'src': url });
+        var img = jQuery('<img>',{ 'src': url.replace('_m.jpg', '_b.jpg') });
 
         jQuery('.bg-squares').eq(id).append(img);
       });
