0eb9648e23e18492e4337aa625a5b438ad215646
John Grande <jgrande@visualjazz.com>
2014-05-23 20:39:11 +0800

    new changes

--- js/insta.js
+++ js/insta.js
@@ -1,39 +1,40 @@
 core = core || {};
 
 core.insta = function(){
-	this.objects = {
-		elements: jQuery('.bg-squares'),
-		background: jQuery('.background'),
-		frame: jQuery('.chosen'),
-		activeClass: 'background-active'
-	};
+  this.objects = {
+    elements: jQuery('.bg-squares'),
+    background: jQuery('.background'),
+    frame: jQuery('.chosen'),
+    activeClass: 'background-active'
+  };
 }
 
 core.insta.prototype.bindEvents = function(){
-	var obj = this.objects;
+  var obj = this.objects;
 
-	function runEvents(event){
-		event.preventDefault();
+  (function($,w){
+    $(obj.elements).click(this.frameEvents.runEvents);
+    $(obj.frame).click(this.frameEvents.turnBackEvents);
+  })(jQuery,window);
+}
 
-		var self = jQuery(this),
-			imgSrc = self.find('img').prop('src'),
-			insertedImg = jQuery('<img>',{ 'src': imgSrc.replace('_m.jpg','_z.jpg') });
+core.insta.prototype.frameEvents = {
+  runEvents: function (event){
+    event.preventDefault();
 
-		obj['background'].addClass(obj.activeClass);
-		obj['frame'].html(insertedImg);
-		obj['frame'].removeClass(obj.activeClass);
+    var self = jQuery(this),
+        imgSrc = self.find('img').prop('src'),
+        insertedImg = jQuery('<img>',{ 'src': imgSrc.replace('_m.jpg','_z.jpg') });
 
-	}
+    obj['background'].addClass(obj.activeClass);
+    obj['frame'].html(insertedImg);
+    obj['frame'].removeClass(obj.activeClass);
 
-	function turnBackEvents(event){
-		event.preventDefault();
+  },
+  turnBackEvents: function(event){
+    event.preventDefault();
 
-		obj['background'].removeClass(obj.activeClass);
-		obj['frame'].addClass(obj.activeClass);
-	}
-
-	(function($,w){
-		$(obj.elements).click(runEvents);
-		$(obj.frame).click(turnBackEvents);
-	})(jQuery,window);
+    obj['background'].removeClass(obj.activeClass);
+    obj['frame'].addClass(obj.activeClass);
+  }
 }
