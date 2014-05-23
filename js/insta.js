c2ecb71831d31f14270d830597602a32026211cf
John Grande <jgrande@visualjazz.com>
2014-05-23 08:23:05 +0800

    new changes

--- js/insta.js
+++ js/insta.js
@@ -1,3 +1,30 @@
-var INSTA = INSTA || {};
+core = core || {};
 
-INSTA.clid = "NTA0YzUzMDEzZjY2NGI4ZjkzMWJmY2I1YTZkNzJhOTA=";
+core.insta = function(){
+	this.objects = {
+		elements: jQuery('.bg-squares'),
+		background: jQuery('.background'),
+		frame: jQuery('.chosen'),
+		activeClass: 'background-active'
+	};
+}
+
+core.prototype.bindEvents = function(){
+	var obj = this.objects;
+
+	function runEvents(event){
+		event.preventDefault();
+
+		var self = jQuery(this),
+			imgSrc = self.find('img').prop('src'),
+			insertedImg = jQuery('<img>',{ 'src': imgSrc.replace('_m.jpg','_b.jpg') });
+
+		obj['background'].addClass(obj.activeClass);
+		obj['chosen'].empty().append(insertedImg);
+
+	}
+
+	(function($,w){
+		$(obj.elements).click(runEvents);
+	})(jQuery,window);
+}
