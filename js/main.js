1be38c64b9aab83c1a12ed496acf0a8d931c0482
John Grande <jgrande@visualjazz.com>
2014-05-24 03:20:49 +0800

    debug

--- js/main.js
+++ js/main.js
@@ -4,13 +4,14 @@ core.home = function(){
   return {
     init: function(){
       console && console.log ? console.log("thedorkjedi - github version 1.00") : window.log("thedorkjedi - github version 1.00");
+
       //set background
       var background = new core.dimensions();
       background.setSquares();
 
-      //set events
+      // set events
       var events = new core.insta();
-      // events.bindEvents();
+      events.bindEvents();
     }
   }
 }
