95958c41cc200b5a50bdede3a3ccc92f55ccda95
John Grande <jgrande@visualjazz.com>
2014-05-23 22:12:56 +0800

    new changes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ core = core || {};
 core.home = function(){
   return {
     init: function(){
-      console && console.log ? console.log("thedorkjedi - github version 1.00");
+      console && console.log ? console.log("thedorkjedi - github version 1.00") : window.log("thedorkjedi - github version 1.00");
       //set background
       var background = new core.dimensions();
       background.setSquares();
